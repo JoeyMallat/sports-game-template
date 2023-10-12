05902d8af0f51b34c191ab5917ccd98f3af0493a
JoeyMallat <joeymallat.business@hotmail.com>
2023-10-12 15:26:57 +0200

    Playoff Viewer script

---
+++ SportsGameTemplate/Assets/PlayoffViewer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayoffViewer : MonoBehaviour, ISettable
+{
+    public void SetDetails<T>(T item) where T : class
+    {
+        
+    }
+}
